46f379af2dc6ebabdbb79400c80f63c51870440a
Nuwin Vinwath <131539550+nuwin2003@users.noreply.github.com>
2024-06-09 11:26:56 +0530

    remove the server config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5176
-  },
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5176
+  // },
   base: '/my-portfolio/',
   plugins: [react()],
 })
